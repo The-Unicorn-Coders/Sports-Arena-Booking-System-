027c27ee281c7b50bc6cc7f9098c38b8f25a1e57
Hirumalshi95 <gunawardhanath.20@uom.lk>
2023-01-26 08:38:43 +0530

    Updated css in comment box

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+            <div class="comment-box">
+     
+     <h1>Write a review</h1>
+       
+       <label> Description </label>
+        
+      <form class="comment-form">
+           <textarea rows="10" placeholder="Write your comment"></textarea>
+          <button type='submit'class='submit-btn'>submit</button>
+      </form>
+      </div>
     </div>
   );
 }
